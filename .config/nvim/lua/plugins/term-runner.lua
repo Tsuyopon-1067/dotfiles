6415b329a49c47d64d13368740398d11d739d4f3
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-04-15 13:44:23 +0900

    ✨ allow to run rust program

--- .config/nvim/lua/plugins/term-runner.lua
+++ .config/nvim/lua/plugins/term-runner.lua
@@ -28,7 +28,42 @@ elseif ext == 'py' then
 elseif ext == 'lua' then
   ex = 'lua ' .. vim.fn.expand '%:p'
 elseif ext == 'rs' then
-  ex = 'cd ../&&cargo run --bin ' .. vim.fn.expand '%:r'
+  local current_directory = vim.api.nvim_eval "expand('%:p:h')"
+  local current_parts = {}
+  for part in current_directory:gmatch '[^/]+' do
+    table.insert(current_parts, part)
+  end
+  local file_full_path = vim.api.nvim_eval "expand('%:p')"
+  local file_parts = {}
+  for part in file_full_path:gmatch '[^/]+' do
+    table.insert(file_parts, part)
+  end
+  -- srcの1つ上のディレクトリに移動する
+  -- 開いているファイルからsrcディレクトリまでいくつ上か調べる
+  local offset_file_src = 0
+  while file_parts[#file_parts - offset_file_src] ~= 'src' and offset_file_src <= #file_parts do
+    offset_file_src = offset_file_src + 1
+  end
+  -- 今のファイル分1つ大きいので修正する
+  offset_file_src = offset_file_src - 1
+  -- srcの1つ上のディレクトリ名がcargo_name
+  local cargo_name = current_parts[#current_parts - offset_file_src - 1]
+  local cd_commands = 'cd ..'
+  local offset_current_src = offset_file_src - (#file_parts - #current_parts)
+  if offset_current_src >= 1 then
+    local i = offset_file_src
+    while i > 1 do
+      cd_commands = cd_commands .. '/..'
+      i = i - 1
+    end
+  else
+    cd_commands = ''
+  end
+  if file_parts[#file_parts - offset_file_src] == 'bin' then
+    ex = cd_commands .. 'cargo run --bin ' .. vim.fn.expand '%:t:r'
+  else
+    ex = cd_commands .. 'cargo run --bin ' .. cargo_name
+  end
 elseif ext == 'js' then
   ex = 'node ' .. vim.fn.expand '%:p'
 end

95a55ab0f1b1084acfc68e685b14f183ccf5b2ed
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-06-27 16:21:01 +0900

    ✨ add allacritty and zellij

--- fetchChange.sh
+++ fetchChange.sh
@@ -4,3 +4,7 @@ mkdir -p ./.config/nvim/
 cp -a ~/.config/nvim/ ./.config/nvim/
 rm -f .zshrc
 cp ~/.zshrc .zshrc
+rm -rf .config/alacritty
+cp -a ~/.config/alacritty/ ./.config/alacritty/
+rm -rf .config/zellij/
+cp -a ~/.config/zellij/ ./.config/zellij/

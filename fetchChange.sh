ad68627b1ad571a76d63f705ead70e928ddd9ce2
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-11-07 10:20:35 +0900

    ✨ switch update brew dump

--- fetchChange.sh
+++ fetchChange.sh
@@ -19,6 +19,18 @@ rm -rf ./.config/karabiner
 mkdir -p ./.config/karabiner
 cp -a ~/.config/karabiner/ ./.config/karabiner/
 
-rm -f .Brewfile
-brew bundle dump -f --global
-cp -a ~/.Brewfile ./.Brewfile
+
+has_brew_option=false
+for arg in "$@"; do
+    if [ "$arg" = "--brew" ]; then
+        has_brew_option=true
+        break
+    fi
+done
+
+if $has_brew_option; then
+    echo "dump brew packages"
+    rm -f .Brewfile
+    brew bundle dump -f --global
+    cp -a ~/.Brewfile ./.Brewfile
+fi

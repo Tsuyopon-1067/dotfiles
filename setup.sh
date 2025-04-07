c6f4a0542702e5804a4e9864440d1f4919cc12be
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-04-07 09:52:01 +0900

    ✨ starship settings

--- setup.sh
+++ setup.sh
@@ -10,5 +10,6 @@ cp -a ./.config/nvim/ ~/.config/nvim/
 cp -a ./.config/alacritty/ ~/.config/alacritty/
 cp -a ./.config/zellij/ ~/.config/zellij/
 cp -a ./.config/karabiner/ ~/.config/karabiner/
+cp -a ./.config/starship.toml ~/.config/starship.toml
 
 curl -sS https://starship.rs/install.sh | sh

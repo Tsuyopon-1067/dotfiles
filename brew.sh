69a30859f055bbb7dc377743b76e9a0b7c5bba03
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-04-07 09:48:54 +0900

    🩹 fix cask option

--- brew.sh
+++ brew.sh
@@ -2,16 +2,20 @@
 
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask alacritty
+brew install zsh-autosuggestions
+brew install zsh-completion
+brew install font-hackgen-nerd
 brew install --cask firefox
 brew install --cask visual-studio-code
 brew install --cask gimp
 brew install --cask thunderbird
 brew install neovim
+brew install --cask google-japanese-ime
 brew install --cask inkscape
 brew install --cask karabiner-elements
 brew install --cask discord
 brew install --cask docker
-brew cask install google-japanese-ime
 brew install --cask zoom
 brew install --cask google-chrome
 brew install go
@@ -20,7 +24,6 @@ brew install --cask vlc
 brew install --cask android-studio
 brew install --cask sf-symbols
 brew install --cask slack
-brew install --cask alacritty
 brew install --cask skim
 brew install typst
 brew install --cask clipy
@@ -28,6 +31,3 @@ brew install --cask postman
 brew install --cask blender
 brew install --cask audacity
 brew install gcc
-brew install font-hackgen-nerd
-brew install zsh-autosuggestions
-brew install zsh-completion
